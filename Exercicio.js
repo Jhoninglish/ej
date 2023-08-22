ade7dc28bb61e06864803c4152faffd3d61ed505
Jhoninglish <142617436+Jhoninglish@users.noreply.github.com>
2023-08-22 17:25:04 -0300

    Create Exercicio.js

---
+++ Exercicio.js
@@ -0,0 +1,53 @@
+/*
+* João Vítor da Silva Rosa
+*
+*/
+
+/*
+//1. resposta: Nesse primeiro código, fara aparecer o número 10 que é let a = 10 na tela e mais o número 5 que é console.log(b).
+
+//2. resposta: Nesse código, primeirante pegara o número 10 e 20 e fará "Uma junção de ambos" numa "caixa" só que não na A ou B,
+mas na C.
+
+//3. resposta: Nesse código ele pergunta Horas de trabalho, Quanto recebe por dia e depois diz quanto você recebe por hora com
+base no que você respondeu anteriormente.
+
+//4. resposta: Código utilizado para mostrar e perguntar quais quer coisas como ex: Nomes e idades.
+  F: Notei que ele pegou as informações nome e idade e armazenou-as na tela.
+
+//5. Código para mostrar perguntas e respostas abaixo:
+
+
+let meuNome;
+let minhaIdade;
+
+console.log;(typeof meuNome);
+console.log;(typeof minhaIdade);
+
+
+
+let roupa = prompt("Você está usando uma camisa verde?");
+let trabalho = prompt("Você gosta do seu trabalho");
+let comeu = prompt("Você comeu hoje?");
+
+console.log(typeof roupa);
+console.log(typeof trabalho);
+console.log(typeof comeu);
+console.log(roupa,trabalho,comeu);
+
+*/
+
+//6. resposta abaixo:
+
+let A = 10
+let B = 25
+let C;
+
+C = A;
+A = B;
+B = C;
+
+console.log(A);
+console.log(B);
+console.log(C);
+
